e76a10171124302281c604bc8f649c0cd9bf2a2a
Pavel Maslov <pmaslov@gmail.com>
2022-04-14 22:37:48 -0400

    fixed some units tests

--- src/app/new-item/new-item.component.spec.ts
+++ src/app/new-item/new-item.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StockItem } from './../model/StockItem';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { NewItemComponent } from './new-item.component';
 
@@ -8,9 +11,9 @@ describe('NewItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NewItemComponent ]
-    })
-    .compileComponents();
+      declarations: [NewItemComponent],
+      providers: [FormBuilder, { provide: MatDialogRef, useValue: {} }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +25,16 @@ describe('NewItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(
+    'populated form is valid',
+    <any>fakeAsync(() => {
+      const testStock = new StockItem('MICROSOFT', 'MSFT', 100, 'NASDAQ', true);
+      component.formGroup.controls['company'].setValue(testStock.company);
+      component.formGroup.controls['ticker'].setValue(testStock.ticker);
+      component.formGroup.controls['price'].setValue(testStock.price);
+      component.formGroup.controls['exchange'].setValue(testStock.exchange);
+      expect(component.formGroup.valid).toEqual(true);
+    })
+  );
 });
